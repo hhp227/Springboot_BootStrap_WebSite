48816f638285cc59cf48ebb25065a34ef0c83268
hhp227 <hong227@naver.com>
2019-11-11 01:56:31 +0900

    Initial Project

---
+++ src/main/java/kr/hong227/springboot/controller/HomeController.java
@@ -0,0 +1,12 @@
+package kr.hong227.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/main/java/kr/hong227/springboot/mapper/HomeMapper.java
@@ -0,0 +1,8 @@
+package kr.hong227.springboot.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HomeMapper {
+
+}
