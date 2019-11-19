a7a11cb57011113e27e139e74f5fa3814b561064
hhp227 <hong227@naver.com>
2019-11-19 16:32:05 +0900

    2019.11.19 commit

--- src/main/java/kr/hhp227/springboot/controller/HomeController.java
+++ src/main/java/kr/hhp227/springboot/controller/HomeController.java
@@ -9,11 +9,46 @@ import java.util.Map;
 
 @Controller
 public class HomeController {
+
     @RequestMapping("/")
     public String home(ModelMap modelMap) {
         Map<String, String> viewBag = new HashMap<>();
         viewBag.put("Title", "Home Page");
         modelMap.addAttribute("ViewBag", viewBag);
         return "home";
+    }
+
+    @RequestMapping("About")
+    public String about(ModelMap modelMap) {
+        Map<String, String> viewBag = new HashMap<>();
+        viewBag.put("Title", "About");
+        viewBag.put("Message", "Your application description page.");
+        modelMap.addAttribute("ViewBag", viewBag);
+        return "about";
+    }
+
+    @RequestMapping("Contact")
+    public String contact(ModelMap modelMap) {
+        Map<String, String> viewBag = new HashMap<>();
+        viewBag.put("Title", "Contact");
+        viewBag.put("Message", "Your contact page.");
+        modelMap.addAttribute("ViewBag", viewBag);
+        return "contact";
+    }
+
+    @RequestMapping("Login")
+    public String login(ModelMap modelMap) {
+        Map<String, String> viewBag = new HashMap<>();
+        viewBag.put("Title", "로그인");
+        modelMap.addAttribute("ViewBag", viewBag);
+        return "login";
+    }
+
+    @RequestMapping("Register")
+    public String register(ModelMap modelMap) {
+        Map<String, String> viewBag = new HashMap<>();
+        viewBag.put("Title", "등록");
+        modelMap.addAttribute("ViewBag", viewBag);
+        return "register";
     }
 }
