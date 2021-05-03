890547c90b7fcb7899ab6a3e59d36fc89f091aa0
hhp227 <hong227@naver.com>
2021-05-03 09:09:58 +0900

    2021.05.03 Commit

--- src/main/java/kr/hhp227/springboot/controller/ManageController.java
+++ src/main/java/kr/hhp227/springboot/controller/ManageController.java
@@ -4,10 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class ManageController {
     @RequestMapping("Manage")
     public String manage(ModelMap modelMap) {
+        Map<String, String> viewBag = new HashMap<>();
+
+        viewBag.put("Title", "Manage");
+        viewBag.put("StatusMessage", "Temp");
+        modelMap.addAttribute("ViewBag", viewBag);
         return "manage/index";
     }
 }
--- src/main/webapp/WEB-INF/jsp/manage/index.jsp
+++ src/main/webapp/WEB-INF/jsp/manage/index.jsp
@@ -1,12 +1,8 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Title</title>
-</head>
-<body>
 
-</body>
-</html>
+<h2>${ViewBag.Title}</h2>
+
+<p class="text-success">${ViewBag.StatusMessage}</p>
 
 <!--
 @model WebApplication1.Models.IndexViewModel
