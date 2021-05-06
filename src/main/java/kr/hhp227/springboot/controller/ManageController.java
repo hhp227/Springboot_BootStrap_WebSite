8b5fc7bfcab187817b79ace7be177e95853dc9bb
hhp227 <hong227@naver.com>
2021-05-06 16:57:19 +0900

    2021.05.06 Commit

--- src/main/java/kr/hhp227/springboot/controller/ManageController.java
+++ src/main/java/kr/hhp227/springboot/controller/ManageController.java
@@ -1,6 +1,5 @@
 package kr.hhp227.springboot.controller;
 
-import org.graalvm.compiler.hotspot.replacements.HubGetClassNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
